57ada0ee06fac472f777d2eb5732c646d7530315
Alexander Doronin <14823403+aledoronin@users.noreply.github.com>
2023-05-29 11:53:28 -0500

    Delete Build directory

--- Build/Intermediates.noindex/Neu(t)ralMC.build/Debug/Neu(t)ralMC.build/Objects-normal/arm64/MetalMC.d
+++
@@ -1,3 +0,0 @@
-dependencies: /Users/alexd/Dropbox/NMC/Neu(t)ralMCCode/MetalMC.m \
-  /Users/alexd/Dropbox/NMC/Neu(t)ralMCCode/MetalMC.h \
-  /Users/alexd/Dropbox/NMC/Neu(t)ralMCCode/definitions.h
--- Build/Intermediates.noindex/Neu(t)ralMC.build/Release/Neu(t)ralMC.build/Objects-normal/x86_64/main.d
+++
@@ -1,4 +0,0 @@
-dependencies: /Users/alexd/Dropbox/NMC/Neu(t)ralMCCode/main.m \
-  /Users/alexd/Dropbox/NMC/Neu(t)ralMCCode/MetalMC.h \
-  /Users/alexd/Dropbox/NMC/Neu(t)ralMCCode/definitions.h \
-  /Users/alexd/Dropbox/NMC/Neu(t)ralMCCode/mcio.h
